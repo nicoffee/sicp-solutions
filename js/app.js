83d616ae585215159116f6116d706ca1cf9d62ed
nicoffee <fedoseevn@gmail.com>
2017-04-15 13:58:48 +0500

    step #4

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
+var myNews = [
+  {
+    author: 'Саша Печкин',
+    text: 'В четверг, четвертого числа...'
+  },
+  {
+    author: 'Просто Вася',
+    text: 'Считаю, что $ должен стоить 35 рублей!'
+  },
+  {
+    author: 'Гость',
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+  }
+];
+
 var News = React.createClass({
   render: function() {
+    var data = this.props.data;
+    var newsTemplate = data.map(function(item, index) {
+      return (
+        <div key={index}>
+          <p className="news__author">{item.author}:</p>
+          <p className="news__text">{item.text}</p>
+        </div>
+      );
+    })
+
     return (
       <div className="news">
-        К сожалению, новостей нет.
+        {newsTemplate}
       </div>
     );
   }
@@ -13,7 +38,7 @@ var App = React.createClass({
     return (
       <div className="app">
         Всем привет, я компонент App! Я умею отображать новости.
-        <News />
+        <News data={myNews}/>. {/*добавили свойство data */}
         <Comments />
       </div>
     );
