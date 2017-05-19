a46aee73b78427dfaad4ebd9644f377628ca0637
nicoffee <fedoseevn@gmail.com>
2017-05-19 09:59:40 +0500

    added state

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 var my_news = [
   {
     author: 'Саша Печкин',
-    text: 'В четчерг, четвертого числа...'
+    text: 'В четчерг, четвертого числа...',
+    bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
   },
   {
     author: 'Просто Вася',
-    text: 'Считаю, что $ должен стоить 35 рублей!'
+    text: 'Считаю, что $ должен стоить 35 рублей!',
+    bigText: 'А евро 42!'
   },
   {
     author: 'Гость',
-    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+    bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
   }
 ];
 
@@ -17,17 +20,30 @@ var Article = React.createClass({
   propTypes: {
     data: React.PropTypes.shape({
       author: React.PropTypes.string.isRequired,
-      text: React.PropTypes.string.isRequired
+      text: React.PropTypes.string.isRequired,
+      bigText: React.PropTypes.string.isRequired
     })
+  },
+  getInitialState: function() {
+    return {
+      visible: false
+    }
+  },
+  readmoreClick: function() {
+    this.setState({visible: true});
   },
   render: function() {
     var author = this.props.data.author,
-        text = this.props.data.text;
+        text = this.props.data.text,
+        bigText = this.props.data.bigText,
+        visible = this.state.visible;
 
     return (
       <div className='article'>
         <p className='news__author'>{author}:</p>
         <p className='news__text'>{text}</p>
+        <a href="#" onClick={this.readmoreClick} className={'news__readmore ' + (visible ? 'none' : '')}>Подробнее</a>
+        <p className={'news__big-text ' + (visible ? '' : 'none')}>{bigText}</p>
       </div>
     )
   }
