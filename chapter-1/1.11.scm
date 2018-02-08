faf90b694bd37249dfc41f3dcbef7a93e56bd20c
nicoffee <fedoseevn@gmail.com>
2018-02-08 23:29:13 +0500

    Fix solution

--- chapter-1/1.11.scm
+++ chapter-1/1.11.scm
@@ -1,15 +1,18 @@
+#lang racket
+
 ;;; recursive process
 (define (f n)
-    (if (< n 3) n
-        (+ (f (- n 1))
-                    (* (f (- n 2)) 2)
-                        (* (f (- n 3)) 3))))
+  (if (< n 3) n
+    (+ (f (- n 1))
+      (* 2 (f (- n 2)))
+      (* 3 (f (- n 3))))))
                   
 ;;; iterative process
-(define (f n)
+(define (f2 n)
     (f-iter 0 1 2 n))
 
 (define (f-iter a b c count)
-    (if (< count 3)
-        c
+    (if (= count 0)
+        a
         (f-iter b c (+ (* a 3) (* b 2) c) (- count 1))))
+
