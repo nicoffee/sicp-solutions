cf3fb34a8ed5153f05c72f90dc5f7f5d76639c18
Nikolay Fedoseev <fedoseevn@gmail.com>
2018-09-04 18:19:36 +0300

    Add 2.2 solution

---
+++ chapter-2/2.2.rkt
@@ -0,0 +1,33 @@
+#lang racket
+
+(define (make-segment a b)
+  (cons a b))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point x) (car x))
+
+(define (y-point x) (cdr x))
+
+(define (midpoint-segment segment)
+  (cons (/ (+ (x-point (start-segment segment)) (x-point (end-segment segment))) 2)
+        (/ (+ (y-point (start-segment segment)) (y-point (end-segment segment))) 2)))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define A (make-point 0 0))
+(define B (make-point 4 2))
+(define AB (make-segment A B))
